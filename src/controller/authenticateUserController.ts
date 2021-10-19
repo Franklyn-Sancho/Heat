a121df9a32bbd5f655ab43f312d099692f1f347c
Franklyn-sancho <franklyn.hs@gmail.com>
2021-10-19 20:49:48 -0300

    autenticação de usuário com JWT, rotas, tratativa de erros

--- src/controller/authenticateUserController.ts
+++ src/controller/authenticateUserController.ts
@@ -1,15 +1,18 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
 import { AuthenticateUserService } from '../services/AuthenticateUserService'
 
 class AuthenticateUserController {
     async handle(request: Request, response: Response) {
-        const {code} = request.body;
-        
-        const service = new AuthenticateUserService();
-        const result = await service.execute(code)
+        const { code } = request.body;
 
-        return response.json(result)
+        const service = new AuthenticateUserService();
+        try {
+            const result = await service.execute(code)
+            return response.json(result)
+        } catch (err) {
+            return response.json(err.message)
+        }
     }
 }
 
-export {AuthenticateUserController}
+export { AuthenticateUserController }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
+import prismaClient from "../prisma"
+import {sign} from "jsonwebtoken"
 
 interface IAccessTokenResponse {
     access_token: string
@@ -15,7 +17,7 @@ class AuthenticateUserService {
     async execute(code: string) {
         const url = "https://github.com/login/oauth/access_token"
 
-        const {data: accessTokenResponse} = await axios.post<IAccessTokenResponse>(url, null, {
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -32,8 +34,41 @@ class AuthenticateUserService {
             }
         })
 
-        return response.data;
+        const { login, id, avatar_url, name } = response.data
+
+        let user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        })
+        if(!user) {
+            user = await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
+        }
+
+        const token = sign({
+            user: {
+                name: user.name,
+                avatar_url: user.avatar_url,
+                id: user_id,
+            }, 
+        },
+        process.env.JWT_SECRET,
+        {
+            subject: user.id,
+            expiresIn: "1d",
+        }
+        
+        )
+
+        return {token, user};
     }
 }
 
-export {AuthenticateUserService}
+export { AuthenticateUserService }
